435dfc39427682c6097502fd1d49a9fde59790f0
Nambroa <nambroa@eryxsoluciones.com.ar>
2018-10-05 00:46:59 -0300

    Maximum Depth of Binary Tree

--- trees/warmup/A_maximum_depth_of_binary_tree/algorithm.py
+++ trees/warmup/A_maximum_depth_of_binary_tree/algorithm.py
@@ -15,3 +15,31 @@
 #    15   7
 # return its depth = 3.
 
+# For this exercise, we will be using DFS Inorder as our tree traversal method.
+
+
+def get_list_of_depths(tree_node, list_of_depths, current_depth):
+    if tree_node is not None:
+        list_of_depths.append(current_depth)
+        get_list_of_depths(tree_node.left, list_of_depths, current_depth+1)
+        print("Visiting {0}".format(tree_node.val))
+        print("Current Depth: {0}".format(current_depth))
+        get_list_of_depths(tree_node.right, list_of_depths, current_depth+1)
+    return max(list_of_depths)
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def max_depth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return get_list_of_depths(root, [0], 1)
