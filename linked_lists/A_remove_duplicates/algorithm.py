306cbc72d4162f309f7b07fadc3f9932deeacbd4
Nambroa <nambroa@eryxsoluciones.com.ar>
2019-03-13 16:07:20 -0300

    Added explanation for remove_duplicates exercise (Linked Lists)

--- linked_lists/A_remove_duplicates/algorithm.py
+++ linked_lists/A_remove_duplicates/algorithm.py
@@ -13,6 +13,11 @@ QUESTIONS:
 
 """
 
+# The main idea is to iterate over the linked list, having a set where we store the values we iterate over.
+# If we find a repeated value, we must point the previous node to the next node of the current node in the iteration.
+# Basically, I have: previous node --> current node (to remove) --> next node
+#        And I need: previous node --> next node
+
 
 def remove_duplicates_from(linked_list):
     if linked_list.length() == 0:
@@ -23,8 +28,6 @@ def remove_duplicates_from(linked_list):
     while current_node is not None:
         if current_node.data() in unique_nodes:
             # Delete the node from the linked list.
-            # I have: previous node --> current node (to remove) --> next node
-            # I need: previous node --> next node
             previous_node.set_next_element(current_node.next_element())
             if current_node == linked_list.tail():
                 linked_list.set_tail(previous_node)
